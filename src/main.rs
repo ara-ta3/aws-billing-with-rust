9300b205b0e46e43bbe88cb3151e1cb979688a7f
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2024-01-06 15:29:12 +0900

    Hello world with slack webhook

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest;
+use serde_json::json;
+use std::env;
+
+#[tokio::main]
+async fn main() {
+    let webhook_url = env::var("SLACK_WEBHOOK_URL").expect("SLACK_WEBHOOK_URL is not set");
+
+    let payload = json!({
+        "text": "Hello, World from Rust!"
+    });
+
+    match reqwest::Client::new()
+        .post(&webhook_url)
+        .json(&payload)
+        .send()
+        .await
+    {
+        Ok(response) => {
+            if response.status().is_success() {
+                println!("success!");
+            } else {
+                eprintln!("failed. slack api status: {}", response.status());
+            }
+        }
+        Err(e) => {
+            eprintln!("failed. error: {}", e);
+        }
+    }
 }
