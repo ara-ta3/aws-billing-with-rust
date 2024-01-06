6d643f2553d413ee9032f0a02c4d2dfc13dc5181
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2024-01-06 15:33:23 +0900

    Add types for docs

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
-use reqwest;
-use serde_json::json;
+use serde_json::Value;
 use std::env;
+use std::result::Result;
 
 #[tokio::main]
 async fn main() {
     let webhook_url = env::var("SLACK_WEBHOOK_URL").expect("SLACK_WEBHOOK_URL is not set");
 
-    let payload = json!({
+    let payload: Value = serde_json::json!({
         "text": "Hello, World from Rust!"
     });
-
-    match reqwest::Client::new()
+    let response: Result<reqwest::Response, reqwest::Error> = reqwest::Client::new()
         .post(&webhook_url)
         .json(&payload)
         .send()
-        .await
-    {
+        .await;
+
+    match response {
         Ok(response) => {
             if response.status().is_success() {
                 println!("success!");
