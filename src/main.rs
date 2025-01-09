f442548ee5411be5fa41e96a3b605df0dd7cd6a2
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2025-01-09 09:57:00 +0900

    Use anyhow

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,24 @@
 use serde_json::Value;
 use std::env;
-use std::result::Result;
+use anyhow::Result;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     let webhook_url = env::var("SLACK_WEBHOOK_URL").expect("SLACK_WEBHOOK_URL is not set");
 
     let payload: Value = serde_json::json!({
         "text": "Hello, World from Rust!"
     });
-    let response: Result<reqwest::Response, reqwest::Error> = reqwest::Client::new()
+    let response = reqwest::Client::new()
         .post(&webhook_url)
         .json(&payload)
         .send()
-        .await;
+        .await?;
 
-    match response {
-        Ok(response) => {
-            if response.status().is_success() {
-                println!("success!");
-            } else {
-                eprintln!("failed. slack api status: {}", response.status());
-            }
-        }
-        Err(e) => {
-            eprintln!("failed. error: {}", e);
-        }
+    if response.status().is_success() {
+        println!("success!");
+    } else {
+        eprintln!("failed. slack api status: {}", response.status());
     }
+    Ok(())
 }
